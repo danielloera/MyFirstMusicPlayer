f88d59c9dc2ce1468aa5c570ce7a97f11345021a
Daniel Matthew Loera <danielloera@utexas.edu>
2017-04-12 23:23:17 -0500

    added Spotify search activity with dependencies. Skeleton should be complete.

---
+++ app/src/main/java/com/loera/musicdemo/myfirstmusicapp/SpotifySearchActivity.java
@@ -0,0 +1,13 @@
+package com.loera.musicdemo.myfirstmusicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpotifySearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spotify_search);
+    }
+}
